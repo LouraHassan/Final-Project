43911528425f3b48ba431b4689e81898e9a43a32
Lomooon <Alfarsi.loura@gmail.com>
2024-10-21 13:49:19 +0300

    fonts added

--- src/Routers/Routers.jsx
+++ src/Routers/Routers.jsx
@@ -1,11 +1,16 @@
 import { createBrowserRouter } from "react-router-dom";
 import App from "../App";
+import AdminHomePage from "../Pages/AdminHomePage";
 
 const Router = createBrowserRouter([
 //     {
 //         path: '/',
 //         element: <App></App>
-// }
+    // }
+    {
+        path: '/admin',
+        element: <AdminHomePage></AdminHomePage>
+    }
 ])
 
 export default Router
