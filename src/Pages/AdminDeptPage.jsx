da9770c478cbc15b1192f80f55aae08227187de1
Lomooon <Alfarsi.loura@gmail.com>
2024-10-21 13:35:16 +0300

    main folders commit

---
+++ src/Pages/AdminDeptPage.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function AdminDeptPage() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default AdminDeptPage
+
+
+// token for admin, 
+// all departments, notifications, 
