312a6a741658c868a64e7fec0d20d2bb85427b60
Lomooon <Alfarsi.loura@gmail.com>
2024-10-30 08:55:17 +0300

    hover removed

--- src/Components/HomeComponents/FeaturesCard.jsx
+++ src/Components/HomeComponents/FeaturesCard.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function FeaturesCard(props) {
   return (
-    <div className="p-5 bg-secondary text-secondary-content flex flex-col items-center rounded shadow justify-between transition ease-out hover:-translate-y-1 hover:scale-110 duration-300">
+    <div className="p-5 bg-secondary text-secondary-content flex flex-col items-center rounded shadow justify-between ">
    {props.icon}
     <p className="text-md font-title font-bold m-2 text-center">
       {props.title}
